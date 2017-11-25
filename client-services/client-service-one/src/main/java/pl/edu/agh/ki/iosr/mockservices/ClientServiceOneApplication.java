04c92223c80b375dee1714e0110acd560dd2d64f
Wojciech Dymek <wojciechdymek94@gmail.com>
2017-11-25 13:09:44 +0100

    fixed build

--- client-services/client-service-one/src/main/java/pl/edu/agh/ki/iosr/mockservices/ClientServiceOneApplication.java
+++ client-services/client-service-one/src/main/java/pl/edu/agh/ki/iosr/mockservices/ClientServiceOneApplication.java
@@ -1,60 +1,18 @@
 package pl.edu.agh.ki.iosr.mockservices;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.sleuth.metric.SpanMetricReporter;
-import org.springframework.cloud.sleuth.zipkin.HttpZipkinSpanReporter;
-import org.springframework.cloud.sleuth.zipkin.ZipkinAutoConfiguration;
-import org.springframework.cloud.sleuth.zipkin.ZipkinProperties;
-import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
-
-import zipkin.Span;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @Configuration
-@AutoConfigureBefore(ZipkinAutoConfiguration.class)
 @ComponentScan("pl.edu.agh.ki.iosr.mockservices")
 public class ClientServiceOneApplication {
-	
-	@Autowired
-	private EurekaClient eurekaClient;
-
-	@Autowired
-	private SpanMetricReporter spanMetricReporter;
-
-	@Autowired
-	private ZipkinProperties zipkinProperties;
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ClientServiceOneApplication.class, args);
     }
-    
-    @Bean
-	public ZipkinSpanReporter makeZipkinSpanReporter() {
-		return new ZipkinSpanReporter() {
-			private HttpZipkinSpanReporter delegate;
-			private String baseUrl;
-
-			@Override
-			public void report(Span span) {
-				InstanceInfo instance = eurekaClient.getNextServerFromEureka("zipkin", false);
-				baseUrl = instance.getHomePageUrl();
-				delegate = new HttpZipkinSpanReporter(new RestTemplate(), baseUrl, zipkinProperties.getFlushInterval(),
-						spanMetricReporter);
-
-				delegate.report(span);
-			}
-		};
-	}
 }
--- computing-services/integral-service/src/main/java/pl/edu/agh/ki/iosr/mockservices/IntegralServiceApplication.java
+++ computing-services/integral-service/src/main/java/pl/edu/agh/ki/iosr/mockservices/IntegralServiceApplication.java
@@ -1,56 +1,18 @@
 package pl.edu.agh.ki.iosr.mockservices;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.sleuth.metric.SpanMetricReporter;
-import org.springframework.cloud.sleuth.zipkin.HttpZipkinSpanReporter;
-import org.springframework.cloud.sleuth.zipkin.ZipkinProperties;
-import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.client.RestTemplate;
-
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
-
-import zipkin.Span;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan({"pl.edu.agh.ki.iosr.mockservices.boundary", "pl.edu.agh.ki.iosr.mockservices.control"})
 public class IntegralServiceApplication {
-	
-	@Autowired
-	private EurekaClient eurekaClient;
-
-	@Autowired
-	private SpanMetricReporter spanMetricReporter;
-
-	@Autowired
-	private ZipkinProperties zipkinProperties;
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(IntegralServiceApplication.class, args);
     }
     
-    @Bean
-	public ZipkinSpanReporter makeZipkinSpanReporter() {
-		return new ZipkinSpanReporter() {
-			private HttpZipkinSpanReporter delegate;
-			private String baseUrl;
-
-			@Override
-			public void report(Span span) {
-				InstanceInfo instance = eurekaClient.getNextServerFromEureka("zipkin", false);
-				baseUrl = instance.getHomePageUrl();
-				delegate = new HttpZipkinSpanReporter(new RestTemplate(), baseUrl, zipkinProperties.getFlushInterval(),
-						spanMetricReporter);
-
-				delegate.report(span);
-			}
-		};
-	}
 }
