13a6e78c046f3e2b9fadf5cc49c201b4f181fcc9
beatabubula <beatabubula@interia.pl>
2017-11-29 17:54:26 +0100

    Add integration test script

--- test.sh
+++ test.sh
@@ -1,8 +1,47 @@
-sleep 200
-if curl client-service-one/random | grep -q 'is equal to'; then
-  echo "TEST PASSED!!!!"
-  exit 0
-else
-  echo "TEST FAILED!!!!"
-  exit 1
+test1=false
+test2=false
+test3=false
+
+for (( i=1; i<=20; i++)) do
+  if ! $test1 ; then
+      if curl client-service-one:8181/random | grep -q 'is equal to'; then
+        echo "client-service-one/random test passed"
+        test1=true
+      fi
+  fi
+
+  if ! $test2 ; then
+      if curl client-service-one:8181/integral | grep -q 'is equal to'; then
+        echo "client-service-one/integral test passed"
+        test2=true
+      fi
+  fi
+
+  if ! $test3 ; then
+      if curl client-service-two:8282/generate-tasks | grep -q 'Executes'; then
+          echo "client-service-two/generate-tasks test passed"
+          test3=true
+      fi
+  fi
+
+  if $test1 && $test2 && $test3; then
+      echo "All tests passed"
+      exit 0
+  fi
+
+  sleep 10
+done
+
+if ! $tes1 ; then
+    echo "client-service-one/integral test failed"
 fi
+
+if ! $tes2 ; then
+    echo "client-service-one/random test failed"
+fi
+
+if ! $tes3 ; then
+    echo "client-service-two/generate-tasks test failed"
+fi
+
+exit 1
